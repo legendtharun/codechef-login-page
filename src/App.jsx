62a04cc82a9bc11a57e9ab73a834e0e1254634f4
legendtharun <manikandan211205@gmail.com>
2024-06-27 14:18:52 +0530

    seventh one

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Login from "./components/LoginPage.jsx";
 import Login1 from "./components/LoginPage1.jsx"
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/codechef-login-page/'}>
     <Routes>
       
         
